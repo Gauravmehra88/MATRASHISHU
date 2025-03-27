608bdb48574bad281a9da0362cf4e93613282f6b
Gauravmehra88 <151906528+Gauravmehra88@users.noreply.github.com>
2025-03-27 23:27:14 +0530

    MediVirtuoso_Vision_Pro.py

--- MATRASHISHU/pages/MediVirtuoso_Vision_Pro.py
+++ MATRASHISHU/pages/MediVirtuoso_Vision_Pro.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Read API key from environment variable
-app_key = "AIzaSyCKD47HOv99aWEyvJcoPZzfSetSDAIxF-U"
+app_key = "AIzaSyBue5KqNeZoqOD9L8r2d0oQPEtSd3qYGM8"
 
 st.set_page_config(
     page_title="Swast_AI",
